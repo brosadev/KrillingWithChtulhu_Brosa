26a43861c5f6c3b57cc88d8aca4aff2877390663
Christopher Tevren <chrtev@gmail.com>
2023-12-04 20:29:08 -0800

    more formatting fix

--- src/display/mod.rs
+++ src/display/mod.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 
-use crate::GameState;
 use crate::display::systems::{DisplayData, DisplayEvent};
+use crate::GameState;
 
 use self::systems::{setup, update_score};
 
@@ -11,9 +11,9 @@ pub struct DisplayPlugin;
 
 impl Plugin for DisplayPlugin {
     fn build(&self, app: &mut App) {
-				app.insert_resource(DisplayData { total_score: 0 })
-        	.add_systems(OnEnter(GameState::Active), setup)
-					.add_event::<DisplayEvent>()
-          .add_systems(Update, update_score);
+        app.insert_resource(DisplayData { total_score: 0 })
+            .add_systems(OnEnter(GameState::Active), setup)
+            .add_event::<DisplayEvent>()
+            .add_systems(Update, update_score);
     }
 }
--- src/display/systems.rs
+++ src/display/systems.rs
@@ -1,12 +1,12 @@
 /* EXAMPLE fn: how to add a score event to any system */
 /*
 pub fn how_to_use(
-	mut score_event: EventWriter<DisplayEvent>,
-	input: Res<Input<KeyCode>>,
-	) {
-	if input.pressed(KeyCode::E) {
-		score_event.send(DisplayEvent { points: 1 });
-	}
+    mut score_event: EventWriter<DisplayEvent>,
+    input: Res<Input<KeyCode>>,
+    ) {
+    if input.pressed(KeyCode::E) {
+        score_event.send(DisplayEvent { points: 1 });
+    }
 }***********************************/
 
 use bevy::prelude::*;
@@ -15,53 +15,50 @@ use bevy::prelude::*;
 pub struct ScoreText;
 
 #[derive(Resource, Debug, Default)]
-pub struct DisplayData{
-	pub total_score: usize,
+pub struct DisplayData {
+    pub total_score: usize,
 }
 
 #[derive(Event, Default)]
-pub struct DisplayEvent{
-	pub points: usize,
+pub struct DisplayEvent {
+    pub points: usize,
 }
 
-pub fn setup(
-	mut commands: Commands,
-	) {
-	commands.spawn((
-		TextBundle::from_sections([
-			TextSection::new(		
-				"Score: ",
-				TextStyle {	
-					font_size: 25.0,
-					..default()
-				}),
-			TextSection::from_style( 
-				TextStyle {	
-					font_size: 25.0,
-					..default()
-				})
-				,
-			])
-			.with_text_alignment(TextAlignment::Center)
-			.with_style(Style {
-				position_type: PositionType::Absolute,
-				bottom: Val::Px(5.0),
-				right: Val::Px(5.0),
-				..default()
-			}),
-			ScoreText,
-	));
+pub fn setup(mut commands: Commands) {
+    commands.spawn((
+        TextBundle::from_sections([
+            TextSection::new(
+                "Score: ",
+                TextStyle {
+                    font_size: 25.0,
+                    ..default()
+                },
+            ),
+            TextSection::from_style(TextStyle {
+                font_size: 25.0,
+                ..default()
+            }),
+        ])
+        .with_text_alignment(TextAlignment::Center)
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(5.0),
+            right: Val::Px(5.0),
+            ..default()
+        }),
+        ScoreText,
+    ));
 }
 
 pub fn update_score(
-	mut score_events: EventReader<DisplayEvent>,
-	mut query: Query<&mut Text, With<ScoreText>>,
-	mut display_data: ResMut<DisplayData>
-	) {
-	for mut score in &mut query {
-		for event in score_events.read() {
-			display_data.total_score += event.points;
-			score.sections[1].value = format!("{}", display_data.total_score);
-		}
-	}
+    mut score_events: EventReader<DisplayEvent>,
+    mut query: Query<&mut Text, With<ScoreText>>,
+    mut display_data: ResMut<DisplayData>,
+) {
+    for mut score in &mut query {
+        for event in score_events.read() {
+            display_data.total_score += event.points;
+            score.sections[1].value = format!("{}", display_data.total_score);
+        }
+    }
 }
