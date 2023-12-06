6218b25a5aee2313de8a3095d2c1f4d13c8b4d81
Brandon Rosario <brosadev@hotmail.com>
2023-12-05 18:20:35 -0800

    Bunch o krill

--- src/krill/systems.rs
+++ src/krill/systems.rs
@@ -1,6 +1,13 @@
-use bevy::prelude::*;
+use std::ops::Range;
 
-use crate::{assets::ImageAssets, DebugEvent};
+use bevy::prelude::*;
+use rand::prelude::*;
+
+use crate::{
+    assets::ImageAssets,
+    map::{BOTTOM_BORDER, LEFT_BORDER, RIGHT_BORDER, TOP_BORDER},
+    DebugEvent,
+};
 
 #[derive(Clone, Eq, PartialEq, Debug, Default, Component)]
 pub struct Krill;
@@ -17,29 +24,33 @@ pub enum KrillState {
     Idle,
 }
 
-pub fn spawn_krill(
-    mut commands: Commands,
-    // asset_server: Res<AssetServer>,
-    image_assets: Res<ImageAssets>,
-) {
-    commands.spawn((
-        KrillBundle {
-            krill: Krill,
-            sprite: SpriteBundle {
-                sprite: Sprite {
-                    custom_size: Some(Vec2::new(10., 10.)),
+pub fn spawn_krill(mut commands: Commands, image_assets: Res<ImageAssets>) {
+    const STARTING_KRILL: u16 = 150;
+    const SPAWN_X_RANGE: Range<f32> = LEFT_BORDER..RIGHT_BORDER;
+    const SPAWN_Y_RANGE: Range<f32> = BOTTOM_BORDER..TOP_BORDER;
+    let mut rand_gen = thread_rng();
+
+    for _ in 0..STARTING_KRILL {
+        let random_x = rand_gen.gen_range(SPAWN_X_RANGE);
+        let random_y = rand_gen.gen_range(SPAWN_Y_RANGE);
+        commands.spawn((
+            KrillBundle {
+                krill: Krill,
+                sprite: SpriteBundle {
+                    sprite: Sprite {
+                        custom_size: Some(Vec2::new(3., 3.)),
+                        ..Default::default()
+                    },
+                    texture: image_assets.krill.clone(),
+                    transform: Transform::from_translation(Vec3::new(random_x, random_y, 1.)),
                     ..Default::default()
                 },
-                texture: image_assets.krill.clone(),
-                transform: Transform {
-                    ..Default::default()
-                },
-                ..Default::default()
             },
-        },
-        Name::new("Krill"),
-    ));
+            Name::new("Krill"),
+        ));
+    }
 }
+
 pub fn debug_krill(
     mut debug_event_reader: EventReader<DebugEvent>,
     krill_query: Query<&Transform, With<Krill>>,
@@ -54,12 +65,14 @@ pub fn debug_krill(
 
 pub fn krill_idle_movement(mut krill_query: Query<&mut Transform, With<Krill>>, time: Res<Time>) {
     for mut krill_transform in krill_query.iter_mut() {
-        const IDLE_HIEGHT_SCALAR: f32 = 0.02;
-        const IDLE_FREQ_SCALAR: f32 = 0.2;
+        const IDLE_HIEGHT_SCALAR: f32 = 0.005;
+        const IDLE_FREQ_SCALAR: f32 = 0.1;
 
         // info!("{:?}", (time.elapsed_seconds() / IDLE_FREQ_SCALAR).sin() * IDLE_HIEGHT_SCALAR;);
 
-        krill_transform.translation.y +=
-            (time.elapsed_seconds() / IDLE_FREQ_SCALAR).sin() * IDLE_HIEGHT_SCALAR;
+        krill_transform.translation.y += (time.elapsed_seconds()
+            - (krill_transform.translation.x / 5.) / IDLE_FREQ_SCALAR)
+            .sin()
+            * IDLE_HIEGHT_SCALAR;
     }
 }
